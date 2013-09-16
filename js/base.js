e3f9d5d757c5eb6847a4a4070ffbe1b315bac498
Grant Schulte <schulte.grant@gmailcom>
2013-09-15 21:00:43 -0400

    Added User Ctrl Homepage

--- js/base.js
+++ js/base.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
 	// smooth scroll
 
-	
 	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
 	    e.preventDefault();
 	    var target = this.hash;
@@ -13,8 +12,6 @@ $(document).ready(function() {
 	        window.location.hash = target;
 	    });
 	});
-
-	
 
 	// sticky menu
 
@@ -39,20 +36,20 @@ $(document).ready(function() {
 	var score_panel = $('.user-score');
 
 	nav.waypoint({
-			handler: function(direction) {
+		handler: function(direction) {
 
-        		if (direction == 'down'){
-        			nav.addClass('sticky');
-        			left_panel.css({ 'height' : nav.outerHeight() });
-        			score_panel.slideUp();
-        		} else {
-        			nav.removeClass('sticky');
-        			left_panel.css({ 'height' : 'auto' });
-        			score_panel.slideDown();
-        		}
-        		
+    		if (direction == 'down'){
+    			nav.addClass('sticky');
+    			left_panel.css({ 'height' : nav.outerHeight() });
+    			score_panel.slideUp();
+    		} else {
+    			nav.removeClass('sticky');
+    			left_panel.css({ 'height' : 'auto' });
+    			score_panel.slideDown();
     		}
-		});
+    		
+		}
+	});
 
 	var sections = $('section');
   	var navigation_links = $('.user-nav li a');
@@ -62,7 +59,7 @@ $(document).ready(function() {
 		
 		var active_section;
 		active_section = $(this);
-
+		
 		if (direction === "up") active_section = active_section.prev();
 
 		var active_link = $('.user-nav li a[href="#' + active_section.attr("id") + '"]');
