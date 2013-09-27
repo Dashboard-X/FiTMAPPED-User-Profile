21971eb5131e517795733c2932d827b054fcc478
Grant Schulte <schulte.grant@gmailcom>
2013-09-26 23:39:30 -0400

    Input on/change action additions.  Adds friend search input.

--- js/base.js
+++ js/base.js
@@ -2,19 +2,25 @@ $(document).ready(function() {
 
 	// Datepicker
 
-	$('.selectDate').datepicker({ dateFormat: 'DD, M dd' })
-					.val($.datepicker.formatDate('DD, M dd', new Date()));
+	$('.selectDate').datepicker({ dateFormat: 'DD, M dd'}).val($.datepicker.formatDate('DD, M dd', new Date()));
 
-    // Actions
+    // Form Actions
+
+    $.fn.savedChanges = function(){
+		$(this).html('<i class="icon-ok"></i> Changes Saved').css('background-color','#1bb9b5');
+	};
+
+    function btnOriginal(){
+    	var btnOrig = $('.btn-save');
+    	btnOrig.html('Save Changes').css('background-color','#0069a0');
+    }
 
 	$('.btn-save').click(function(){
 		$(this).savedChanges();
 	});
 
-	$.fn.savedChanges = function(){
-		$(this).html('<i class="icon-ok"></i> Changes Saved')
-			   .css('background-color','##1bb9b5');
-	};
+	$('form').on('input', btnOriginal);
+	$('select, checkbox, radio, .selectDate').change(btnOriginal);
 
 	// Smooth Scroll
 
