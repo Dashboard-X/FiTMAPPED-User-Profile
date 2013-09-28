e880d743bc916fb7133f29c867d1130b3a7d4703
grantschulte <schulte.grant@gmail.com>
2013-09-28 10:27:40 -0400

    Fixes js conflicts.  Adjusts icon sizes

--- js/base.js
+++ js/base.js
@@ -1,27 +1,5 @@
 $(document).ready(function() {
 
-	// Datepicker
-
-	$('.selectDate').datepicker({ dateFormat: 'DD, M dd'}).val($.datepicker.formatDate('DD, M dd', new Date()));
-
-    // Form Actions
-
-    $.fn.savedChanges = function(){
-		$(this).html('<i class="icon-ok"></i> Changes Saved').css('background-color','#1bb9b5');
-	};
-
-    function btnOriginal(){
-    	var btnOrig = $('.btn-save');
-    	btnOrig.html('Save Changes').css('background-color','#0069a0');
-    }
-
-	$('.btn-save').click(function(){
-		$(this).savedChanges();
-	});
-
-	$('form').on('input', btnOriginal);
-	$('select, checkbox, radio, .selectDate').change(btnOriginal);
-
 	// Smooth Scroll
 
 	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
